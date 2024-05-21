5c9f3aaeadf8fe4939ddf2561b21fc28e5160f54
nyaneet <nyaneet@gmail.com>
2024-05-21 19:05:32 +0300

    feat: add form field for file

---
+++ lib/core/models/file/csv_ut8_file.dart
@@ -0,0 +1,27 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:sss_data_upload_client/core/models/file/file.dart';
+/// Representation of uploaded [File]
+/// with csv format and utf8 character encoding
+final class CsvUtf8File implements File<String> {
+  final String _name;
+  final Uint8List _bytes;
+  /// Creates [File] with csv format and utf8 character encoding
+  /// using passed [name] and [bytes]
+  const CsvUtf8File({
+    required String name,
+    required Uint8List bytes,
+  })  : _name = name,
+        _bytes = bytes;
+  //
+  @override
+  String name() => _name;
+  //
+  @override
+  List<int> bytes() => _bytes.toList();
+  /// Returns [CsvUtf8File] content as text
+  @override
+  extract() {
+    return utf8.decode(_bytes);
+  }
+}
